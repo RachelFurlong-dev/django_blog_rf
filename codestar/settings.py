4db1c9fc6d3ea3d6ff602b5c2ed3cb8eef423fac
Rachel Dev <racheldesignuk@gmail.com>
2026-06-10 15:07:15 +0100

    Move secret key to environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+if os.path.exists("env.py"):
+    import env
 import dj_database_url
 from pathlib import Path
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#1!$cjhb66g@aip126fe)3-j4my%6db=lq8af!!+)g(sz1itvg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
